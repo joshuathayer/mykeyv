d4804bc9b85e2fe74f67625e29505349c33c395f
Joshua Thayer <joshua@Joshua-Thayers-MacBook-Pro.local>
2009-12-15 14:42:22 -0800

    clearer example for storableObject

--- bin/storableObject.pl
+++ bin/storableObject.pl
@@ -8,11 +8,13 @@ sub new {
 
 	my $self = $class->SUPER::new();
 
+	# hints to mykeyv regarding how to store instances of this object
         $self->{'_mykv_storable_make_key'} = sub { return "fakeuser_object_".$self->{first}; };
         $self->{'_mykv_storable_object_vars'} = ['first','last','friends'];
+
+	# users have a first name, a last name, and a list of friends
         $self->{'first'} = '';
         $self->{'last'} = '';
-
         $self->{'friends'} = [];
 
 	bless($self, $class);
@@ -21,8 +23,6 @@ sub new {
 
 sub mykv_remote_append {
 	my ($self, $args) = @_;
-
-	print "wow wtf! do you see me?\n";
 
 	unshift(@{$self->{friends}}, $args->{name});
 
@@ -47,29 +47,24 @@ my $cluster = $Config::cluster;
 my $cluster_state = $Config::cluster_state;
 my $pending_cluster = $Config::pending_cluster;
 
-print "cluster state $cluster_state\n";
-print Dumper $cluster;
-
 my $kvc = Mykeyv::MyKVClient->new({
 	cluster => $cluster,
-	pending_cluster => $pending_cluster,
 	cluster_state => $cluster_state,
 });
 
-#my $u = FakeUser->new();
-#$u->{'first'} = "jerry";
-#$u->{'last'} = "garcia";
-
-#my $cv = AnyEvent->condvar;
-#$u->mykvStore(sub{$cv->send});
-#$cv->recv;
+# create and store a jerry object.
+my $u = FakeUser->new();
+$u->{'first'} = "jerry";
+$u->{'last'} = "garcia";
 
 my $cv = AnyEvent->condvar;
-my $w = FakeUser->new();
-$w->{'first'} = "jerry";
-$w->mykvRestore(sub{$cv->send});
+$u->mykvStore(sub{$cv->send});
 $cv->recv;
-print "jerry's last name is $w->{last}\n";      # prints "jerry's last name is garcia"
+
+# ...later (or in another piece of code)...
+
+# add joshua to the list of jerry's friends. recall, this
+# actually takes place on the remote kvd server
 my $cv = AnyEvent->condvar;
-$w->mykv_remote_append({name=>"joshua"}, sub {$cv->send});
+$u->mykv_remote_append({name=>"joshua"}, sub {$cv->send});
 $cv->recv;
